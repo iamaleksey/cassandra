1744ff2318187f1f3a839f0b2f8c33820bf91256
Dinesh A. Joshi <dinesh.joshi@apple.com>
2018-07-03 12:07:11 -0700

    Optimize Streaming

--- src/java/org/apache/cassandra/io/sstable/Component.java
+++ src/java/org/apache/cassandra/io/sstable/Component.java
@@ -33,6 +33,10 @@ public class Component
 
     final static EnumSet<Type> TYPES = EnumSet.allOf(Type.class);
 
+    /**
+     * WARNING: Be careful while changing the names or string representation of the enum
+     * members. Streaming code depends on the names during streaming (Ref: CASSANDRA-14556).
+     */
     public enum Type
     {
         // the base data for an sstable: the remaining components can be regenerated
@@ -59,13 +63,14 @@ public class Component
         // custom component, used by e.g. custom compaction strategy
         CUSTOM(null);
 
-        final String repr;
+        public final String repr;
+
         Type(String repr)
         {
             this.repr = repr;
         }
 
-        static Type fromRepresentation(String repr)
+        public static Type fromRepresentation(String repr)
         {
             for (Type type : TYPES)
             {
@@ -120,7 +125,7 @@ public class Component
      * @return the component corresponding to {@code name}. Note that this always return a component as an unrecognized
      * name is parsed into a CUSTOM component.
      */
-    static Component parse(String name)
+    public static Component parse(String name)
     {
         Type type = Type.fromRepresentation(name);
 
