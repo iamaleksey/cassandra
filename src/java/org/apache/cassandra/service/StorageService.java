77f1cdaa9c8f8cb7472039fd65058a92b0055eb4
Aleksey Yeshchenko <aleksey@apple.com>
2018-08-29 18:42:39 +0100

    Clean up Message.Request implementations

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -5344,6 +5344,11 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         return traceProbability;
     }
 
+    public boolean shouldTraceProbablistically()
+    {
+        return traceProbability != 0 && ThreadLocalRandom.current().nextDouble() < traceProbability;
+    }
+
     public void disableAutoCompaction(String ks, String... tables) throws IOException
     {
         for (ColumnFamilyStore cfs : getValidColumnFamilies(true, true, ks, tables))
