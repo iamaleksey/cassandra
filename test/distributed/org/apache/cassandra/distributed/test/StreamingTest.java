ee24fde29a488993dec9803b67e7e2c5d812b66f
Benedict Elliott Smith <benedict@apache.org>
2019-03-19 15:33:57 +0000

    improve ported streaming test

--- test/distributed/org/apache/cassandra/distributed/test/StreamingTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/StreamingTest.java
@@ -18,39 +18,56 @@
 
 package org.apache.cassandra.distributed.test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.junit.Assert;
 import org.junit.Test;
 
-import org.apache.cassandra.db.ConsistencyLevel;
 import org.apache.cassandra.distributed.Cluster;
-import org.apache.cassandra.distributed.util.PyDtest;
 import org.apache.cassandra.service.StorageService;
 
 public class StreamingTest extends DistributedTestBase
 {
 
-    private void testStreaming(int nodes, int replicationFactor, int rowCount) throws Throwable
+    private void testStreaming(int nodes, int replicationFactor, int rowCount, String compactionStrategy) throws Throwable
     {
         try (Cluster cluster = Cluster.create(nodes))
         {
             cluster.schemaChange("CREATE KEYSPACE " + KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': " + replicationFactor + "};");
-            cluster.schemaChange(PyDtest.createCf(KEYSPACE, "cf")
-                                        .withColumns("c1:text", "c2:text")
-                                        .withCompactionStrategy("LeveledCompactionStrategy")
-                                        .build());
+            cluster.schemaChange(String.format("CREATE TABLE %s.cf (k text, c1 text, c2 text, PRIMARY KEY (k)) WITH compaction = {'class': '%s', 'enabled': 'true'}", KEYSPACE, compactionStrategy));
 
             for (int i = 0 ; i < rowCount ; ++i)
-                cluster.coordinator(1).execute(String.format("INSERT INTO %s.cf (key, c1, c2) VALUES (?, 'value1', 'value2');", KEYSPACE), ConsistencyLevel.ALL, Integer.toString(i));
+            {
+                for (int n = 1 ; n < nodes ; ++n)
+                    cluster.get(n).executeInternal(String.format("INSERT INTO %s.cf (k, c1, c2) VALUES (?, 'value1', 'value2');", KEYSPACE), Integer.toString(i));
+            }
 
-            cluster.get(2).executeInternal("TRUNCATE system.available_ranges;");
+            cluster.get(nodes).executeInternal("TRUNCATE system.available_ranges;");
+            {
+                Object[][] results = cluster.get(nodes).executeInternal(String.format("SELECT k, c1, c2 FROM %s.cf;", KEYSPACE));
+                Assert.assertEquals(0, results.length);
+            }
 
-            cluster.get(2).runOnInstance(() -> StorageService.instance.rebuild(null, KEYSPACE, null, null));
+            cluster.get(nodes).runOnInstance(() -> StorageService.instance.rebuild(null, KEYSPACE, null, null));
+            {
+                Object[][] results = cluster.get(nodes).executeInternal(String.format("SELECT k, c1, c2 FROM %s.cf;", KEYSPACE));
+                Assert.assertEquals(1000, results.length);
+                Arrays.sort(results, Comparator.comparingInt(a -> Integer.parseInt((String) a[0])));
+                for (int i = 0 ; i < results.length ; ++i)
+                {
+                    Assert.assertEquals(Integer.toString(i), results[i][0]);
+                    Assert.assertEquals("value1", results[i][1]);
+                    Assert.assertEquals("value2", results[i][2]);
+                }
+            }
         }
     }
 
     @Test
     public void test() throws Throwable
     {
-        testStreaming(2, 2, 1000);
+        testStreaming(2, 2, 1000, "LeveledCompactionStrategy");
     }
 
 }
