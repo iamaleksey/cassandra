c8ec69bab4985d653c465bcde13e3edda5be6179
Aleksey Yeshchenko <aleksey@apache.org>
2019-07-02 10:10:19 +0100

    wip

--- src/java/org/apache/cassandra/net/SocketFactory.java
+++ src/java/org/apache/cassandra/net/SocketFactory.java
@@ -18,13 +18,14 @@
 package org.apache.cassandra.net;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.nio.channels.ClosedChannelException;
+import java.nio.channels.spi.SelectorProvider;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeoutException;
 import javax.annotation.Nullable;
 import javax.net.ssl.SSLEngine;
@@ -38,9 +39,9 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.DefaultSelectStrategyFactory;
 import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.MultithreadEventLoopGroup;
 import io.netty.channel.SingleThreadEventLoop;
 import io.netty.channel.epoll.EpollChannelOption;
 import io.netty.channel.epoll.EpollEventLoopGroup;
@@ -53,10 +54,10 @@ import io.netty.channel.unix.Errors;
 import io.netty.handler.ssl.OpenSsl;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslHandler;
+import io.netty.util.concurrent.DefaultEventExecutorChooserFactory;
 import io.netty.util.concurrent.DefaultThreadFactory;
-import io.netty.util.concurrent.EventExecutor;
-import io.netty.util.concurrent.MultithreadEventExecutorGroup;
-import io.netty.util.concurrent.SingleThreadEventExecutor;
+import io.netty.util.concurrent.RejectedExecutionHandlers;
+import io.netty.util.concurrent.ThreadPerTaskExecutor;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 import io.netty.util.internal.logging.Slf4JLoggerFactory;
 import org.apache.cassandra.concurrent.NamedThreadFactory;
@@ -82,8 +83,57 @@ public final class SocketFactory
 
     private static final int EVENT_THREADS = Integer.getInteger(Config.PROPERTY_PREFIX + "internode-event-threads", FBUtilities.getAvailableProcessors());
 
-    public enum Provider { EPOLL, NIO }
-    private static final Provider DEFAULT_PROVIDER = NativeTransportService.useEpoll() ? Provider.EPOLL : Provider.NIO;
+    /**
+     * The default task queue used by {@code NioEventLoop} and {@code EpollEventLoop} is {@code MpscUnboundedArrayQueue},
+     * provided by JCTools. While efficient, it has an undesirable quality for a queue backing an event loop: it is
+     * not non-blocking, and can cause the event loop to busy-spin while waiting for a partially completed task
+     * offer, if the producer thread has been suspended mid-offer.
+     *
+     * As it happens, however, we have an MPSC queue implementation that is perfectly fit for this purpose -
+     * {@link ManyToOneConcurrentLinkedQueue}, that is non-blocking, and already used throughout the codebase,
+     * that we can and do use here as well.
+     */
+    enum Provider
+    {
+        NIO
+        {
+            NioEventLoopGroup makeEventLoopGroup(int threadCount, ThreadFactory threadFactory)
+            {
+                return new NioEventLoopGroup(threadCount,
+                                             new ThreadPerTaskExecutor(threadFactory),
+                                             DefaultEventExecutorChooserFactory.INSTANCE,
+                                             SelectorProvider.provider(),
+                                             DefaultSelectStrategyFactory.INSTANCE,
+                                             RejectedExecutionHandlers.reject(),
+                                             capacity -> new ManyToOneConcurrentLinkedQueue<>());
+            }
+        },
+        EPOLL
+        {
+            EpollEventLoopGroup makeEventLoopGroup(int threadCount, ThreadFactory threadFactory)
+            {
+                return new EpollEventLoopGroup(threadCount,
+                                               new ThreadPerTaskExecutor(threadFactory),
+                                               DefaultEventExecutorChooserFactory.INSTANCE,
+                                               DefaultSelectStrategyFactory.INSTANCE,
+                                               RejectedExecutionHandlers.reject(),
+                                               capacity -> new ManyToOneConcurrentLinkedQueue<>());
+            }
+        };
+
+        EventLoopGroup makeEventLoopGroup(int threadCount, String threadNamePrefix)
+        {
+            logger.debug("using netty {} event loop for pool prefix {}", name(), threadNamePrefix);
+            return makeEventLoopGroup(threadCount, new DefaultThreadFactory(threadNamePrefix, true));
+        }
+
+        abstract EventLoopGroup makeEventLoopGroup(int threadCount, ThreadFactory threadFactory);
+
+        static Provider optimalProvider()
+        {
+            return NativeTransportService.useEpoll() ? EPOLL : NIO;
+        }
+    }
 
     /** a useful addition for debugging; simply set to true to get more data in your logs */
     static final boolean WIRETRACE = false;
@@ -99,30 +149,12 @@ public final class SocketFactory
     private final EventLoopGroup outboundStreamingGroup;
     final ExecutorService synchronousWorkExecutor = Executors.newCachedThreadPool(new NamedThreadFactory("Messaging-SynchronousWork"));
 
-    SocketFactory() { this(DEFAULT_PROVIDER); }
+    SocketFactory() { this(Provider.optimalProvider()); }
     SocketFactory(Provider provider)
     {
-        this.acceptGroup = getEventLoopGroup(provider, 1, "Messaging-AcceptLoop");
-        this.defaultGroup = getEventLoopGroup(provider, EVENT_THREADS, NamedThreadFactory.globalPrefix() + "Messaging-EventLoop");
-        this.outboundStreamingGroup = getEventLoopGroup(provider, EVENT_THREADS, "Streaming-EventLoop");
-        assert    provider == providerOf(acceptGroup)
-               && provider == providerOf(defaultGroup)
-               && provider == providerOf(outboundStreamingGroup);
-    }
-
-    private static EventLoopGroup getEventLoopGroup(Provider provider, int threadCount, String threadNamePrefix)
-    {
-        switch (provider)
-        {
-            case EPOLL:
-                logger.debug("using netty epoll event loop for pool prefix {}", threadNamePrefix);
-                return overwriteMPSCQueues(new EpollEventLoopGroup(threadCount, new DefaultThreadFactory(threadNamePrefix, true)));
-            case NIO:
-                logger.debug("using netty nio event loop for pool prefix {}", threadNamePrefix);
-                return overwriteMPSCQueues(new NioEventLoopGroup(threadCount, new DefaultThreadFactory(threadNamePrefix, true)));
-            default:
-                throw new IllegalStateException();
-        }
+        acceptGroup  = provider.makeEventLoopGroup(1, "Messaging-AcceptLoop");
+        defaultGroup = provider.makeEventLoopGroup(EVENT_THREADS, NamedThreadFactory.globalPrefix() + "Messaging-EventLoop");
+        outboundStreamingGroup = provider.makeEventLoopGroup(EVENT_THREADS, "Streaming-EventLoop");
     }
 
     private static Provider providerOf(EventLoopGroup eventLoopGroup)
@@ -269,59 +301,5 @@ public final class SocketFactory
     static String channelId(InetAddressAndPort from, InetAddressAndPort to, ConnectionType type, String id)
     {
         return from + "->" + to + '-' + type + '-' + id;
-    }
-
-    /**
-     * The default task queue used by {@code NioEventLoop} and {@code EpollEventLoop} is {@code MpscUnboundedArrayQueue},
-     * provided by JCTools. While efficient, it has an undesirable quality for a queue backing an event loop: it is
-     * not non-blocking, and can cause the event loop to busy-spin while waiting for a partially completed task
-     * offer, if the producer thread has been suspended mid-offer. Sadly, there is currently no way to work around
-     * this behaviour in application-logic.
-     *
-     * As it happens, however, we have an MPSC queue implementation that is perfectly fit for this purpose -
-     * {@link ManyToOneConcurrentLinkedQueue}, that is non-blocking, and already used throughout the codebase.
-     *
-     * Unfortunately, there is no Netty API or to override the default queue, so we have to resort to reflection,
-     * for now.
-     *
-     * We filed a Netty issue asking for this capability to be provided cleanly:
-     * https://github.com/netty/netty/issues/9105, and hopefully Netty will implement it some day. When and if
-     * that happens, this reflection-based workaround should be removed.
-     */
-    private static EventLoopGroup overwriteMPSCQueues(MultithreadEventLoopGroup eventLoopGroup)
-    {
-        try
-        {
-            for (EventExecutor eventExecutor : (EventExecutor[]) childrenField.get(eventLoopGroup))
-            {
-                SingleThreadEventLoop eventLoop = (SingleThreadEventLoop) eventExecutor;
-                taskQueueField.set(eventLoop, new ManyToOneConcurrentLinkedQueue<>());
-                tailTasksField.set(eventLoop, new ManyToOneConcurrentLinkedQueue<>());
-            }
-            return eventLoopGroup;
-        }
-        catch (IllegalAccessException e)
-        {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    private static final Field childrenField, taskQueueField, tailTasksField;
-    static
-    {
-        try
-        {
-            childrenField = MultithreadEventExecutorGroup.class.getDeclaredField("children");
-            taskQueueField = SingleThreadEventExecutor.class.getDeclaredField("taskQueue");
-            tailTasksField = SingleThreadEventLoop.class.getDeclaredField("tailTasks");
-
-            childrenField.setAccessible(true);
-            taskQueueField.setAccessible(true);
-            tailTasksField.setAccessible(true);
-        }
-        catch (NoSuchFieldException e)
-        {
-            throw new IllegalStateException(e);
-        }
     }
 }
