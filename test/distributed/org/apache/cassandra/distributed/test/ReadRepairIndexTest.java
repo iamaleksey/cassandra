aa551935a54b7ece6c5d5b0ea0a398970fb4bd6c
Blake Eggleston <blake@ultrablake.com>
2025-05-23 13:39:49 -0700

    adding index read repair tests

---
+++ test/distributed/org/apache/cassandra/distributed/test/ReadRepairIndexTest.java
@@ -0,0 +1,172 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.distributed.test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import org.apache.cassandra.distributed.Cluster;
+import org.apache.cassandra.schema.ReplicationType;
+import org.apache.cassandra.service.reads.repair.ReadRepairStrategy;
+
+import static org.apache.cassandra.distributed.shared.AssertUtils.row;
+
+@RunWith(Parameterized.class)
+public class ReadRepairIndexTest extends TestBaseImpl
+{
+    private static final int NUM_NODES = 2;
+    enum IndexType
+    {
+        SECONDARY, SAI
+    }
+
+    /**
+     * The read repair strategy to be used
+     */
+    @Parameterized.Parameter
+    public ReadRepairStrategy strategy;
+
+    /**
+     * The node to be used as coordinator
+     */
+    @Parameterized.Parameter(1)
+    public int coordinator;
+
+    /**
+     * Whether to flush data after mutations
+     */
+    @Parameterized.Parameter(2)
+    public boolean flush;
+
+    /**
+     * Whether paging is used for the distributed queries
+     */
+    @Parameterized.Parameter(3)
+    public boolean paging;
+
+    @Parameterized.Parameter(4)
+    public ReplicationType replicationType;
+
+    @Parameterized.Parameter(5)
+    public IndexType indexType;
+
+
+    @Parameterized.Parameters(name = "{index}: strategy={0} coordinator={1} flush={2} paging={3} replication={4} index={5}")
+    public static Collection<Object[]> data()
+    {
+        List<Object[]> result = new ArrayList<>();
+        for (int coordinator = 1; coordinator <= NUM_NODES; coordinator++)
+            for (boolean flush : BOOLEANS)
+                for (boolean paging : BOOLEANS)
+                    for (ReplicationType replication : ReplicationType.values())
+                        for (IndexType indexType : IndexType.values())
+                            result.add(new Object[]{ ReadRepairStrategy.BLOCKING, coordinator, flush, paging, replication, indexType});
+        for (IndexType indexType : IndexType.values())
+            result.add(new Object[]{ ReadRepairStrategy.NONE, 1, false, false, ReplicationType.untracked, indexType});
+        return result;
+    }
+
+    private static Cluster cluster;
+
+    @BeforeClass
+    public static void setupCluster() throws IOException
+    {
+        cluster = Cluster.build(NUM_NODES)
+                              .withConfig(config -> config.set("read_request_timeout", "1m")
+                                                          .set("write_request_timeout", "1m"))
+                              .start();
+    }
+
+    @AfterClass
+    public static void teardownCluster()
+    {
+        if (cluster != null)
+            cluster.close();
+    }
+
+    protected Tester tester(String restriction)
+    {
+        return new Tester(restriction, cluster, strategy, coordinator, flush, paging, replicationType, indexType);
+    }
+
+    protected static class Tester extends ReadRepairQueryTester.AbstractTester<Tester>
+    {
+        private int nameSeq = 0;
+        private final IndexType indexType;
+
+        public Tester(String restriction, Cluster cluster, ReadRepairStrategy strategy, int coordinator, boolean flush, boolean paging, ReplicationType replicationType, IndexType indexType)
+        {
+            super(restriction, cluster, strategy, coordinator, flush, paging, replicationType);
+            this.indexType = indexType;
+        }
+
+        @Override
+        Tester self()
+        {
+            return this;
+        }
+
+        Tester createIndex(String columns)
+        {
+            String query = String.format("CREATE INDEX %s_index_%d ON %s(v)", tableName, nameSeq++, qualifiedTableName);
+            switch (indexType)
+            {
+                case SECONDARY:
+                    break;
+
+                case SAI:
+                    query = query + " USING 'sai'";
+                    break;
+                default:
+                    throw new AssertionError(indexType);
+            }
+            cluster.schemaChange(query);
+            return this;
+        }
+    }
+
+    /**
+     * A partition that would not be an index hit on one node would be on the other
+     */
+    @Test
+    public void singlePartitionUpdatedPartition()
+    {
+        tester("WHERE k=1 AND v=2")
+        .createTable("CREATE TABLE %s (k int, c int, v int, PRIMARY KEY (k, c))")
+        .createIndex("v")
+        .mutate(1, "INSERT INTO %s (k, c, v) VALUES (1, 1, 1)")
+        .mutate(2, "INSERT INTO %s (k, c, v) VALUES (1, 2, 2)")
+        .queryColumns("k, c, v", 1, 0,
+                      rows(row(1, 2, 2)),
+                      rows(row(1, 2, 2)),
+                      rows(row(1, 2, 2)))
+        .tearDown(1,
+                  rows(row(1, 1, 1), row(1, 2, 2)),
+                  rows(row(1, 2, 2)));
+
+    }
+}
--- test/distributed/org/apache/cassandra/distributed/test/ReadRepairQueryTester.java
+++ test/distributed/org/apache/cassandra/distributed/test/ReadRepairQueryTester.java
@@ -137,23 +137,17 @@ public abstract class ReadRepairQueryTester extends TestBaseImpl
         return new Tester(restriction, cluster, strategy, coordinator, flush, paging, replicationType);
     }
 
-    protected static class Tester extends ReadRepairTester<Tester>
+    static abstract class AbstractTester<T extends AbstractTester<T>> extends ReadRepairTester<T>
     {
         private final String restriction; // the tested CQL query WHERE restriction
         private final String allColumnsQuery; // a SELECT * query for the table using the tested restriction
 
-        Tester(String restriction, Cluster cluster, ReadRepairStrategy strategy, int coordinator, boolean flush, boolean paging, ReplicationType replicationType)
+        AbstractTester(String restriction, Cluster cluster, ReadRepairStrategy strategy, int coordinator, boolean flush, boolean paging, ReplicationType replicationType)
         {
             super(cluster, strategy, coordinator, flush, paging, false, replicationType);
             this.restriction = restriction;
 
             allColumnsQuery = String.format("SELECT * FROM %s %s", qualifiedTableName, restriction);
-        }
-
-        @Override
-        Tester self()
-        {
-            return this;
         }
 
         /**
@@ -168,12 +162,12 @@ public abstract class ReadRepairQueryTester extends TestBaseImpl
          * @param node1Rows                the rows in the first node, which is the one with the most updated data
          * @param node2Rows                the rows in the second node, which is the one meant to receive the RR writes
          */
-        Tester queryColumns(String columns,
-                            long columnsQueryRepairedRows,
-                            long rowsQueryRepairedRows,
-                            Object[][] columnsQueryResults,
-                            Object[][] node1Rows,
-                            Object[][] node2Rows)
+        T queryColumns(String columns,
+                       long columnsQueryRepairedRows,
+                       long rowsQueryRepairedRows,
+                       Object[][] columnsQueryResults,
+                       Object[][] node1Rows,
+                       Object[][] node2Rows)
         {
             // query only the selected columns with CL=ALL to trigger partial read repair on that column
             String columnsQuery = String.format("SELECT %s FROM %s %s", columns, qualifiedTableName, restriction);
@@ -223,13 +217,13 @@ public abstract class ReadRepairQueryTester extends TestBaseImpl
          * @param node1Rows                the rows in the first node, which is the one with the most updated data
          * @param node2Rows                the rows in the second node, which is the one meant to receive the RR writes
          */
-        Tester deleteColumn(String columnDeletion,
-                            String columns,
-                            long columnsQueryRepairedRows,
-                            long rowsQueryRepairedRows,
-                            Object[][] columnsQueryResults,
-                            Object[][] node1Rows,
-                            Object[][] node2Rows)
+        T deleteColumn(String columnDeletion,
+                       String columns,
+                       long columnsQueryRepairedRows,
+                       long rowsQueryRepairedRows,
+                       Object[][] columnsQueryResults,
+                       Object[][] node1Rows,
+                       Object[][] node2Rows)
         {
             assert restriction != null;
 
@@ -250,7 +244,7 @@ public abstract class ReadRepairQueryTester extends TestBaseImpl
          * Executes the specified row deletion on just one node and verifies the tested query, to ensure that the tested
          * query propagates the row deletion.
          */
-        Tester deleteRows(String rowDeletion, long repairedRows, Object[][] node1Rows, Object[][] node2Rows)
+        T deleteRows(String rowDeletion, long repairedRows, Object[][] node1Rows, Object[][] node2Rows)
         {
             mutate(1, rowDeletion);
 
@@ -262,12 +256,12 @@ public abstract class ReadRepairQueryTester extends TestBaseImpl
             return verifyQuery(allColumnsQuery, repairedRows, node1Rows, node2Rows);
         }
 
-        Tester mutate(String... queries)
+        T mutate(String... queries)
         {
             return mutate(1, queries);
         }
 
-        private Tester verifyQuery(String query, long expectedRepairedRows, Object[][] node1Rows, Object[][] node2Rows)
+        private T verifyQuery(String query, long expectedRepairedRows, Object[][] node1Rows, Object[][] node2Rows)
         {
             // verify the per-replica status before running the query distributedly
             assertRows(cluster.get(1).executeInternal(query), node1Rows);
@@ -280,7 +274,7 @@ public abstract class ReadRepairQueryTester extends TestBaseImpl
             assertRows(cluster.get(1).executeInternal(query), node1Rows);
             assertRows(cluster.get(2).executeInternal(query), strategy == NONE ? EMPTY_ROWS : node1Rows);
 
-            return this;
+            return self();
         }
 
         /**
@@ -295,7 +289,7 @@ public abstract class ReadRepairQueryTester extends TestBaseImpl
          * Verifies the final status of the nodes with an unrestricted query, to ensure that the main tested query
          * hasn't triggered any unexpected repairs. Then, it verifies that the node that hasn't been used as coordinator
          * hasn't triggered any unexpected repairs. Finally, it drops the table.
-         *
+         * <p>
          * The expectUnrepaired flag is meant for range query tests where logged replication table special casing
          * doesn't apply since we do expect the final query to find and repair missing mutations
          */
@@ -338,6 +332,20 @@ public abstract class ReadRepairQueryTester extends TestBaseImpl
         void tearDown(long repairedRows, Object[][] node1Rows, Object[][] node2Rows)
         {
             tearDown(repairedRows, node1Rows, node2Rows, false);
+        }
+    }
+
+    protected static class Tester extends AbstractTester<Tester>
+    {
+        public Tester(String restriction, Cluster cluster, ReadRepairStrategy strategy, int coordinator, boolean flush, boolean paging, ReplicationType replicationType)
+        {
+            super(restriction, cluster, strategy, coordinator, flush, paging, replicationType);
+        }
+
+        @Override
+        Tester self()
+        {
+            return this;
         }
     }
 }
